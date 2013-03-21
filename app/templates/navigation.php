554290895e51b5cb23c8fbfdf86188861118c2ad
Warrick Hing <warrickhing@gmail.com>
2013-03-21 22:36:38 +1300

    Removed sushi menu

--- app/templates/navigation.php
+++ app/templates/navigation.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li<?php echo $nav_item==1 ? ' class="selected"':'';?>> <a href="<?php echo SITEROOT ?>">Home</a></li>
 		<li<?php echo $nav_item==2 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>menu/">Menu</a></li>
-		<li<?php echo $nav_item==3 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>menu/sushi-menu/">Sushi Menu</a></li>
+		<!--- <li<?php echo $nav_item==3 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>menu/sushi-menu/">Sushi Menu</a></li> --->
 		<li<?php echo $nav_item==4 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>reservations/">Reservations</a></li>
 		<li<?php echo $nav_item==5 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>functions/">Functions</a></li>
 		<li<?php echo $nav_item==6 ? ' class="selected"':'';?>><a href="<?php echo SITEROOT ?>feedback/">Feedback</a></li>
